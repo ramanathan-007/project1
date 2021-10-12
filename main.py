3c48cf317995d0c93fe4691e7d710357f88f1083
Ramanathan A <80613460+ramanathan-007@users.noreply.github.com>
2021-10-13 01:52:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from sklearn import linear_model
 from sklearn.preprocessing import StandardScaler
 import bar_chart_race
 import plotly
+#svm
+
+import sklearn.metrics
+from sklearn.datasets import load_breast_cancer, load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
 dataset = [5,7,8,7,2,17,2,9,4,11,12,9,6]
 
 x=numpy.mean(dataset)
@@ -94,6 +102,39 @@ print("Scale",scaleda)
 #barchart
 plt.bar(xb,100)
 plt.show()
+
+#support vector Machine.
+
+cancer_data = load_breast_cancer()
+
+x_train, x_test, y_train, y_test = train_test_split(cancer_data.data, cancer_data.target, test_size=0.4, random_state=209)
+
+cls=SVC(kernel="linear")
+
+cls.fit(x_train,y_train)
+
+pred =cls.predict(x_test)
+
+print("accuracy:", sklearn.metrics.accuracy_score(y_test, y_pred=pred))
+
+print("precision:",sklearn.metrics.precision_score(y_test, y_pred=pred))
+
+print("recall:", sklearn.metrics.recall_score(y_test, y_pred=pred))
+
+print(sklearn.metrics.classification_report(y_test, y_pred=pred))
+
+
+
+digits = load_digits()
+
+clf1= SVC(gamma=0.00000000001, C=100)
+x1,y1= digits.data[:10], digits.target[:10]
+clf1.fit(x1,y1)
+
+print(clf1.predict(digits.data[:10]))
+plt.imshow(digits.images[7],interpolation='nearest')
+plt.show()
+
 
 
 
